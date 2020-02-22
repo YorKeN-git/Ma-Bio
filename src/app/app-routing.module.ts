46852599923a4e9752798c7aead54fc71745d06d
YorKeN <dethoor.axel@gmail.com>
2020-02-22 15:32:25 +0100

    WIP page cv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
-import { CvComponent } from './components/cv/cv.component';
+import { CvComponent } from './pages/cv/cv.component';
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import {MenubarModule} from 'primeng/menubar';
 import { IndexComponent } from './pages/index/index.component';
-import { CvComponent } from './components/cv/cv.component';
 import {CardModule} from 'primeng/card';
 import {PanelModule} from 'primeng/panel';
 import {BrowserAnimationsModule} from  '@angular/platform-browser/animations';
+import { CvExperiencesComponent } from './components/cv-experiences/cv-experiences.component';
+import { CvFormationsComponent } from './components/cv-formations/cv-formations.component';
+import { CvTechnoComponent } from './components/cv-techno/cv-techno.component';
+import { CvOutilsComponent } from './components/cv-outils/cv-outils.component';
+import { CvComponent } from './pages/cv/cv.component';
 
 
 @NgModule({
@@ -17,6 +21,11 @@ import {BrowserAnimationsModule} from  '@angular/platform-browser/animations';
     HeaderComponent,
     IndexComponent,
     CvComponent,
+    CvExperiencesComponent,
+    CvFormationsComponent,
+    CvTechnoComponent,
+    CvOutilsComponent,
+    
 
   ],
   imports: [
